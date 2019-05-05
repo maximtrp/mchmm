615749c136b00676851c51e0c23f4467e9879760
maximtrp <maximtrp@gmail.com>
2019-05-05 03:25:50 +0300

    tests upd

--- tests/test_mchmm.py
+++ tests/test_mchmm.py
@@ -16,7 +16,7 @@ class TestMC(unittest.TestCase):
                            [3,2,0]])
 
         a = mc.MarkovChain().from_data(self.seq)
-        self.assertTrue(np.all(a.observed_matrix == result))
+        self.assertTrue(np.allclose(a.observed_matrix, result))
 
     def test_tpm(self):
         '''Checking transition probability matrix'''
@@ -24,7 +24,7 @@ class TestMC(unittest.TestCase):
         result = tfm / tfm.sum(axis=1)[:, None]
 
         a = mc.MarkovChain().from_data(self.seq)
-        self.assertTrue(np.all(a.observed_p_matrix == result))
+        self.assertTrue(np.allclose(a.observed_p_matrix, result))
 
     def test_mcsim(self):
         '''Checking simulation process'''
@@ -33,10 +33,18 @@ class TestMC(unittest.TestCase):
         si2 = a.simulate(15, start=0, ret='indices', seed=11)
         sn2 = a.simulate(15, start=0, ret='states', seed=11)
 
-        self.assertTrue(np.all(si == si2) and np.all(sn == sn2))
+        self.assertTrue(np.allclose(si, si2) and np.all(sn == sn2))
 
     # HMM tests
+    def test_epm(self):
+        '''Checking emission probability matrix'''
+        result = np.array([[0.21052632, 0.21052632, 0.47368421, 0.10526316],
+                           [0.57142857, 0.        , 0.        , 0.42857143]])
 
+        obs_seq = 'AGACTGCATATATAAGGGGCAGGCTG'
+        sts_seq = '00000000111111100000000000'
+        a = hmm.HiddenMarkovModel().from_seq(obs_seq, sts_seq)
+        self.assertTrue(np.allclose(a.ep, result))
 
 if __name__ == '__main__':
     unittest.main()
