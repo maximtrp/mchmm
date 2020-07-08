465030b9e8bf719f2fe5aee6aad8cc6046a0260d
Maksim Terpilowski <maximtrp@gmail.com>
2020-07-09 00:07:24 +0300

    package files updated

--- setup.py
+++ setup.py
@@ -1,40 +1,36 @@
 from setuptools import setup
 from os.path import join, dirname
 
-setup(name='mchmm',
-      version='0.2.0',
-      description='Markov chains and Hidden Markov models',
-      long_description=open(join(dirname(__file__), 'DESCRIPTION.rst')).read(),
-      url='http://github.com/maximtrp/mchmm',
-      author='Maksim Terpilowski',
-      author_email='maximtrp@gmail.com',
-      license='BSD',
-      packages=['mchmm'],
-      keywords='markovchain hmm',
-      install_requires=['numpy', 'scipy'],
-	  classifiers=[
-		'Development Status :: 3 - Alpha',
+setup(
+    name='mchmm',
+    version='0.2.0',
+    description='Markov chains and Hidden Markov models',
+    long_description=open(join(dirname(__file__), 'DESCRIPTION.rst')).read(),
+    url='http://github.com/maximtrp/mchmm',
+    author='Maksim Terpilowski',
+    author_email='maximtrp@gmail.com',
+    license='BSD',
+    packages=['mchmm'],
+    keywords='markov chain hidden markov models',
+    install_requires=['numpy', 'scipy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
 
-		'Intended Audience :: Education',
-		'Intended Audience :: Information Technology',
-		'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
 
-		'Topic :: Scientific/Engineering :: Information Analysis',
-		'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
 
-		'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: BSD License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.1',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-	  ],
-      test_suite='tests.mchmm_suite',
-      zip_safe=False)
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='tests.mchmm_suite',
+    zip_safe=False
+)
