c6dc8ae4d2195c2e1e9ba52ab0ec70337058217a
Maksim Terpilovskii <maximtrp@gmail.com>
2025-09-13 13:30:08 +0200

    bump package version

--- mchmm/__init__.py
+++ mchmm/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.4.5"
-__all__ = ['MarkovChain', 'HiddenMarkovModel']
+__version__ = "0.4.6"
+__all__ = ["MarkovChain", "HiddenMarkovModel"]
 
 from ._hmm import *  # noqa
 from ._mc import *  # noqa
